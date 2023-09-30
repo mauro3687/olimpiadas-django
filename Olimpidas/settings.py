e170b91678452a76b399a3164f06d2682cb5d1c2
Ezequiel Gorosito <mauro.gorosito@25watts.ar>
2023-09-30 18:16:43 -0300

    holaaaaaaaaa

--- Olimpidas/settings.py
+++ Olimpidas/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'Olimpidas/static'),)
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
